ad9e9faa61a6e69e151416f00312f0d1fac80a3b
Anup Agarwal <anupa@andrew.cmu.edu>
2025-07-20 18:30:01 +0000

    Update README

--- plot_all.sh
+++ plot_all.sh
@@ -6,37 +6,46 @@ LOGS=../data/logs
 FIGS=../data/figs
 EXP=frcc-nsdi26
 
-python parse_pcap.py -i ${LOGS}/${EXP}/sweep_bw/ --agg bw_mbps
+# Parsing logs to produce figures
+# Figure 19
 python parse_pcap.py -i ${LOGS}/${EXP}/sweep_flows/ --agg n_flows
+python parse_pcap.py -i ${LOGS}/${EXP}/sweep_bw/ --agg bw_mbps
 python parse_pcap.py -i ${LOGS}/${EXP}/sweep_rtprop/ --agg rtprop_ms
 
+# Figure 20
+python parse_pcap.py -i ${LOGS}/${EXP}/different_rtt_sweep_bw --agg bw_mbps
+python parse_pcap.py -i ${LOGS}/${EXP}/different_rtt/ --agg rtprop_ratio
+
+# Figure 21
 python parse_pcap.py -i ${LOGS}/${EXP}/jitter_sweep_bw --agg bw_mbps
 python parse_pcap.py -i ${LOGS}/${EXP}/jitter --agg jitter_ms
 
-python parse_pcap.py -i ${LOGS}/${EXP}/different_rtt/ --agg rtprop_ratio
-python parse_pcap.py -i ${LOGS}/${EXP}/different_rtt_sweep_bw --agg bw_mbps
-
+# Copying all the relevant figures in one place
 mkdir -p ${FIGS}/${EXP}/evaluation/sweeps/{bw,rtprop,flows,different_rtt,jitter}
 mkdir -p ${FIGS}/${EXP}/evaluation/timeseries
 
+# Figure 19
 cp ${FIGS}/${EXP}/sweep_bw/{xput_ratio.pdf,jfi.pdf,rtt.pdf} ${FIGS}/${EXP}/evaluation/sweeps/bw
 cp ${FIGS}/${EXP}/sweep_flows/{xput_ratio.pdf,jfi.pdf,rtt.pdf} ${FIGS}/${EXP}/evaluation/sweeps/flows
 cp ${FIGS}/${EXP}/sweep_rtprop/{xput_ratio.pdf,jfi.pdf,rtt.pdf} ${FIGS}/${EXP}/evaluation/sweeps/rtprop
 
-cp ${FIGS}/${EXP}/jitter_sweep_bw/{xput_ratio.pdf,jfi.pdf,rtt.pdf} ${FIGS}/${EXP}/evaluation/sweeps/jitter
-cp ${FIGS}/${EXP}/jitter/xput_ratio.pdf ${FIGS}/${EXP}/evaluation/sweeps/jitter/jitter_xput_ratio.pdf
-
+# Figure 20
 cp ${FIGS}/${EXP}/different_rtt_sweep_bw/{xput_ratio.pdf,jfi.pdf,rtt.pdf} ${FIGS}/${EXP}/evaluation/sweeps/different_rtt
 cp ${FIGS}/${EXP}/different_rtt/xput_ratio.pdf ${FIGS}/${EXP}/evaluation/sweeps/different_rtt/rtt_ratio_xput_ratio.pdf
 
-for cca in bbr genericcc_markovian ndd bbr3; do
+# Figure 21
+cp ${FIGS}/${EXP}/jitter_sweep_bw/{xput_ratio.pdf,jfi.pdf,rtt.pdf} ${FIGS}/${EXP}/evaluation/sweeps/jitter
+cp ${FIGS}/${EXP}/jitter/xput_ratio.pdf ${FIGS}/${EXP}/evaluation/sweeps/jitter/jitter_xput_ratio.pdf
+
+# Figures 1, 2, 26, 27
+for cca in bbr genericcc_markovian frcc; do
   cp "${FIGS}/${EXP}/different_rtt_sweep_bw/bw_ppms[6]-ow_delay_ms[5]-n_flows[3]/bw_ppms[6]-ow_delay_ms[5]-n_flows[3]-buf_size_bdp[100]-cca[${cca}]-cca_param_tag[]-rtprop_ratio[2]/tcpdump_throughput.pdf" ${FIGS}/${EXP}/evaluation/timeseries/diff_rtt_${cca}.pdf
 done
 for cca in reno cubic; do
   cp "${FIGS}/${EXP}/different_rtt_sweep_bw/bw_ppms[6]-ow_delay_ms[5]-n_flows[3]/bw_ppms[6]-ow_delay_ms[5]-n_flows[3]-buf_size_bdp[3]-cca[${cca}]-cca_param_tag[]-rtprop_ratio[2]/tcpdump_throughput.pdf" ${FIGS}/${EXP}/evaluation/timeseries/diff_rtt_${cca}.pdf
 done
 
-for cca in bbr genericcc_markovian ndd bbr3; do
+for cca in bbr genericcc_markovian frcc; do
   cp "${FIGS}/${EXP}/jitter_sweep_bw/bw_ppms[8]-ow_delay_ms[16]-n_flows[3]/bw_ppms[8]-ow_delay_ms[16]-n_flows[3]-buf_size_bdp[100]-cca[${cca}]-cca_param_tag[]-jitter_ms[32]-jitter_ppms[32]-jitter_type[fixed_aggregation]/tcpdump_throughput.pdf" ${FIGS}/${EXP}/evaluation/timeseries/jitter_${cca}.pdf
 done
 for cca in reno cubic; do
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 # Install mahimahi and dependencies
-sudo apt-get install autotools-dev autoconf libtool apache2 apache2-dev \
+sudo apt install -y autotools-dev autoconf libtool apache2 apache2-dev \
     protobuf-compiler libprotobuf-dev libssl-dev xcb libxcb-composite0-dev \
     libxcb-present-dev libcairo2-dev libpango1.0-dev gnuplot dnsmasq
 
@@ -19,11 +19,19 @@ make -j
 sudo make install
 cd $cur_dir
 
+# Setup other CCAs (copa)
+## Comment out copa (genericcc_markovian) in sweep.py if you don't want to setup and run copa.
+cd ../ccas/genericCC
+sudo apt install -y g++ makepp libboost-dev libprotobuf-dev protobuf-compiler libjemalloc-dev iperf libboost-python-dev
+makepp
+cd ../cc_bench
+
+# Setup and configure tools
 sudo apt install iperf3 tcpdump
+sudo setcap cap_net_raw,cap_net_admin=eip $(which tcpdump) # allow tcmpdump to capture packets without sudo
 
-sudo mkdir -p /mnt/ramdisk  # For storing live telemetry
-
-sudo setcap cap_net_raw,cap_net_admin=eip $(which tcpdump)
+# Setup in-memory filesystem for live telemetry
+sudo mkdir -p /mnt/ramdisk
 
 exit 0
 }
